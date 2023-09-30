8aebf19e16949e32176bbde2fcac34bb07040106
monajehad <monajehad2000@gmail.com>
2023-09-30 17:34:17 +0300

    front

--- resources/views/backend/winner/select-winner.blade.php
+++ resources/views/backend/winner/select-winner.blade.php
@@ -3,24 +3,37 @@
 @section('content')
 
     <div class="container">
-        <h1>Select Winner</h1>
+        @if(session('success'))
+        <div class="alert alert-success mt-3">
+            {{ session('success') }}
+        </div>
+    @elseif(session('error'))
+    <div class="alert alert-danger mt-3">
+        {{ session('error') }}
+    </div>
+@endif
+<div class=" d-flex" style="justify-content: center;
+align-items: center; margin-top:10%">
+<div>
+    <img class="object-cover object-center rounded" alt="winner" src="{{ asset('img/select.svg') }}">
 
-        <p>Click the button below to select a winner for the campaign:</p>
+</div>
+<div style="
+margin-left: 16px !important;
+">
+    <h3>Winner Selection</h3>
+
+        <p>To select a winner for a campaign kindly follow the following steps.</p>
 
         <form method="POST" action="{{ route('campaigns.do-select-winner', ['campaignId' => $campaignId]) }}">
             @csrf
-            <button type="submit" class="btn btn-primary">Select Winner</button>
+            <button type="submit" style="background:#007DFE" class="btn text-white">Select Winner</button>
         </form>
+</div>
+        
 
-        @if(session('success'))
-            <div class="alert alert-success mt-3">
-                {{ session('success') }}
-            </div>
-        @elseif(session('error'))
-        <div class="alert alert-danger mt-3">
-            {{ session('error') }}
-        </div>
-    @endif
+     
+    </div>
     </div>
 
 
--- resources/views/frontend/show.blade.php
+++ resources/views/frontend/show.blade.php
@@ -1,6 +1,54 @@
 @extends('frontend.layouts.app')
 
 @section('content')
+
+<section class="mb-20" style="background: #C8D7D9;
+height: 670px;
+
+">
+    <div class="container mx-auto flex px-1 sm:px-20 py-20 md:flex-row flex-col items-center">
+
+        <div style="border-radius: 32px !important;
+        height: 480px;" class="w-50 px-4  md:pl-16 flex flex-col  items-center text-center "
+>
+            <img src="{{ asset('storage/'.$campaign->image ?? 'images/def.svg') }}" style="width: 100%;
+            height: 100%;
+            flex-shrink: 0; border-radius: 32px;
+" class="card-img-top" alt="...">
+
+            {{-- <h1 class="title-font sm:text-8xl text-4xl mb-4 font-medium text-gray-800">
+
+                Experience a World of Prizes at Your Fingertips!
+
+            </h1>
+            <p class="mb-8 sm:text-2xl text-2xl">
+                Unleash the thrill of uncertainty, and let fate guide you towards incredible surprises.
+            </p>
+            <div class="flex justify-center">
+                <a href="{{route('show-all-campaigns' )}}"  class="inline-flex btn btn-primary">
+                   
+                    Discover more
+                </a>
+                <a href="{{route('show-all-campaigns' )}}"  class="ml-4 inline-flex btn btn-outline-secondary">
+                   
+                    Our Campaigns 
+                </a> --}}
+            {{-- </div>
+
+            @include('frontend.includes.messages') --}}
+
+        </div>
+
+        <div class="flex flex-col  items-left text-left  w-50">
+            <h2>{{ $campaign->name }}</h2>
+            <p> {{ $campaign->description }}</p>
+
+        </div>
+       
+    </div>
+</section>
+
+{{-- 
     <h1>Campaign Details</h1>
     <img src="{{ asset('storage/'.$campaign->image ?? 'images/def.svg') }}" style="width:200px" class="card-img-top" alt="...">
     <h2>{{ $campaign->name }}</h2>
@@ -40,12 +88,12 @@
         <p>No features available.</p>
     @endif
 <?php
-$ads=App\Models\SmartAds::latest()->take(2)->get();
+// $ads=App\Models\SmartAds::latest()->take(2)->get();
 // dd($ads);
 ?>
 @foreach($ads as $ad)
     <x-smart-ad-component slug="{{$ad->name}}"/>
-@endforeach
+@endforeach --}}
 @endsection
 @push ('after-scripts')
 <script src="{{ asset('vendor/smart-ads/js/smart-banner.min.js') }}"></script>
